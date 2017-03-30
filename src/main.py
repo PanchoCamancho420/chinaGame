1d53bb361335555545f771565205cbf2dae33814
kaihodge <hodgekai@gmail.com>
2017-03-30 11:37:55 -0700

    Fixed file path handeling
    Fixes: #2

--- src/main.py
+++ src/main.py
@@ -18,14 +18,9 @@ from terrainShape import TerrainShape
 
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(relative_path)
 
 
 class World(pyglet.window.Window):
